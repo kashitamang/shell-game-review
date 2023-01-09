bd3c793ee02f0b7867e42323d2816a6b40fbe452
KASHI TAMANG <tamang.kashi@gmail.com>
2023-01-09 11:57:54 -0800

    grabs shells, buttons, and spans from the DOM, and saves wins and losses in state

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 /* Imports */
 
+//shells
+const shell1 = document.getElementById('shell-1');
+const shell2 = document.getElementById('shell-2');
+const shell3 = document.getElementById('shell-3');
+
+//buttons
+const button1 = document.getElementById('button-1');
+const button2 = document.getElementById('button-2');
+const button3 = document.getElementById('button-3');
+
+//spans
+const winSpan = document.getElementById('wins');
+const lossesSpan = document.getElementById('losses');
+const totalSpan = document.getElementById('total');
+
+let wins = 0;
+let losses = 0;
+
 /* Get DOM Elements */
 
 /* State */
