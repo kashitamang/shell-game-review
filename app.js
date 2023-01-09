a167c7a50b16000b7b9816570be3dc6d4784126f
KASHI TAMANG <tamang.kashi@gmail.com>
2023-01-09 13:22:51 -0800

    adding event listener for each button, as well as updateView function and resetStyles function

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-/* Imports */
-
+/* Get DOM Elements */
 //shells
 const shell1 = document.getElementById('shell-1');
 const shell2 = document.getElementById('shell-2');
@@ -18,12 +17,78 @@ const totalSpan = document.getElementById('total');
 let wins = 0;
 let losses = 0;
 
-/* Get DOM Elements */
-
 /* State */
+
+//set state to ball hidden
+
+function resetStyles() {
+    shell1.classList.remove('reveal');
+    shell2.classList.remove('reveal');
+    shell2.classList.remove('reveal');
+}
+
+function updateView() {
+    winSpan.textContent = wins;
+    lossesSpan.textContent = losses;
+    totalSpan.textContent = wins + losses;
+}
 
 /* Events */
 
+button1.addEventListener('click', () => {
+    resetStyles();
+
+    const randomShell = Math.ceil(Math.random() * 3);
+
+    if (randomShell === 1) {
+        shell1.classList.add('reveal');
+        wins++;
+    } else if (randomShell === 2) {
+        shell2.classList.add('reveal');
+        losses++;
+    } else if (randomShell === 3) {
+        shell3.classList.add('reveal');
+        losses++;
+    }
+
+    updateView();
+});
+
+button2.addEventListener('click', () => {
+    resetStyles();
+
+    const randomShell = Math.ceil(Math.random() * 3);
+
+    if (randomShell === 1) {
+        shell1.classList.add('reveal');
+        losses++;
+    } else if (randomShell === 2) {
+        shell2.classList.add('reveal');
+        wins++;
+    } else if (randomShell === 3) {
+        shell3.classList.add('reveal');
+        losses++;
+    }
+    updateView();
+});
+
+button3.addEventListener('click', () => {
+    resetStyles();
+
+    const randomShell = Math.ceil(Math.random() * 3);
+
+    if (randomShell === 1) {
+        shell1.classList.add('reveal');
+        losses++;
+    } else if (randomShell === 2) {
+        shell2.classList.add('reveal');
+        losses++;
+    } else if (randomShell === 3) {
+        shell3.classList.add('reveal');
+        wins++;
+    }
+    updateView();
+});
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
